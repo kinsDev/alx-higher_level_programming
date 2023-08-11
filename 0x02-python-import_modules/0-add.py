683da7432b3b4652eeedca5dc2034b67d343d3ce
kinsDev <kinsleygitonga@gmail.com>
2023-08-11 14:30:46 -0700

    adding comments

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    # Import the add function from add_0 module
     from add_0 import add
 
+    # Assign values to variables a and b
     a = 1
     b = 2
+
+    # Call the add function with a and b as arguments
     result = add(a, b)
+
     print("{:d} + {:d} = {:d}".format(a, b, result))
