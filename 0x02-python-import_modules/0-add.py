d97b7a11c4cdf2f9b7b090610b9ee99a02c84e2b
kinsDev <kinsleygitonga@gmail.com>
2023-08-11 14:22:14 -0700

    a program that imports the function def add(a, b): from the file add_0.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """Print the result of adding 1 and 2"""
-    
-    # Import the add function from add_0.py
     from add_0 import add
-    
-    # Assign values to variables a and b
-    a = 1
-    b = 2
-    
-    # Print the sum of a and b using the add function
-    print("{} + {} = {}".format(a, b, add(a, b)))
+
+    num1 = 1
+    num2 = 2
+    result = add(num1, num2)
+    print("{} + {} = {}".format(num1, num2, result))
