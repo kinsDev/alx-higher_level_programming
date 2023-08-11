d4542e770170dd5c124e0e4bc2cbcde58429f43a
kinsDev <kinsleygitonga@gmail.com>
2023-08-11 14:15:49 -0700

    another correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add as addition_function
+from add_0 import add
 
 a = 1
 b = 2
 
-print("{0} + {1} = {2}".format(a, b, addition_function(a, b)))
+print("{0} + {1} = {2}".format(a, b, add(a, b)))
