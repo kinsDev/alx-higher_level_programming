4f338d8246de8db45e5da1aa63bbee9cfa88d38c
kinsDev <kinsleygitonga@gmail.com>
2023-08-28 13:08:59 -0700

    a function that divides 2 integers and prints the result.

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_division(numerator, denominator):
+    try:
+        result = numerator / denominator  # Try to perform the division
+    except ZeroDivisionError:
+        result = None  # Handle division by zero
+    finally:
+        print("Inside result: {}".format(result))  # Print the result (either division or None)
+    return result
