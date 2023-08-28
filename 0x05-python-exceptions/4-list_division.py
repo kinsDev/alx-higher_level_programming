eb3925e580b4db8d987695ac7a68d1fedd1cbd04
kinsDev <kinsleygitonga@gmail.com>
2023-08-28 13:55:17 -0700

    a function that divides element by element 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
-def list_division(my_list_1, my_list_2, list_length):
-    result_list = []
-    for i in range(list_length):
+def list_division(my_list_a, my_list_b, list_len):
+    new_list = []
+    for i in range(list_len):
         try:
-            division_result = my_list_1[i] / my_list_2[i]
+            quotient = my_list_a[i] / my_list_b[i]
         except (ValueError, TypeError):
             print("wrong type")
-            division_result = 0
+            quotient = 0
         except ZeroDivisionError:
             print("division by 0")
-            division_result = 0
+            quotient = 0
         except IndexError:
             print("out of range")
-            division_result = 0
+            quotient = 0
         finally:
-            result_list.append(division_result)
-    return result_list
-
-my_list_1 = [10, 8, 4]
-my_list_2 = [2, 4, 4]
-result = list_division(my_list_1, my_list_2, max(len(my_list_1), len(my_list_2)))
-print(result)
-
-print("--")
-
-my_list_1 = [10, 8, 4, 4]
-my_list_2 = [2, 0, "H", 2, 7]
-result = list_division(my_list_1, my_list_2, max(len(my_list_1), len(my_list_2)))
-print(result)
+            new_list.append(quotient)
+    return new_list
