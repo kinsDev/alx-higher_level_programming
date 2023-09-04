699ad4a0e9bfd29618c0effea21dee9feaee0e05
kinsDev <kinsleygitonga@gmail.com>
2023-09-04 03:17:09 -0700

    a class Rectangle that defines a rectangle by: (based on 1-rectangle.py)

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,11 +1,24 @@
+#!/usr/bin/python3
+"""
+This script defines a Rectangle class to represent rectangles.
+"""
+
 class Rectangle:
+    """
+    Represents a rectangle.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
+
     def __init__(self, width=0, height=0):
         """
         Initializes a new Rectangle instance.
 
         Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
         self.width = width
         self.height = height
@@ -61,7 +74,7 @@ class Rectangle:
         Returns:
             int: The area of the rectangle.
         """
-        return self.width * self.height
+        return (self.__width * self.__height)
 
     def perimeter(self):
         """
@@ -70,6 +83,6 @@ class Rectangle:
         Returns:
             int: The perimeter of the rectangle.
         """
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.width + self.height)
+        return ((self.__width * 2) + (self.__height * 2))
