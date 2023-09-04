ec38e8bd10e797afd86655012cf2faf3c93e13cd
kinsDev <kinsleygitonga@gmail.com>
2023-09-04 02:32:47 -0700

    Write an empty class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Defines a Rectangle class that can be used to represent and manipulate rectangles.
+"""
+
+class Rectangle:
+    """
+    Represents a rectangle with attributes such as width and height.
+    This class can be used for various rectangle-related operations.
+    """
+    pass
